49faa62aa28b1ffba2f9d2fea0af5de23f0aced8
Daniel Zhu <daniel.zhu@pomona.edu>
2014-12-25 16:35:34 -0800

    Created the project through Eclipse and added the objectdraw library.

---
+++ src/NBAStatsViewer.java
@@ -0,0 +1,15 @@
+import objectdraw.*;
+
+/**
+ * This is the main controller for NBAStatsViewer. The applet is initialized
+ * here and calls the begin() method.
+ * 
+ * @author Daniel Zhu
+ *
+ */
+public class NBAStatsViewer extends WindowController {
+
+	public void begin() {
+
+	}
+}
